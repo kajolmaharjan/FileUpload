5dc5c2004afe19edc8e509ffba7a549fa26fda00
Kajol Maharjan <kajol.maharjan@amniltech.com>
2020-05-22 15:14:40 +0545

    Initial Commit

---
+++ FileUploadTest/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileUploadTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            try
+            {
+                if (file.ContentLength > 0)
+                {
+                    string _FileName = Path.GetFileName(file.FileName);
+                    string _path = Path.Combine(Server.MapPath("~/App_Data/uploads"), _FileName);
+                    file.SaveAs(_path);
+                }
+                ViewBag.Message = "File Uploaded Successfully!!";
+                return View();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                return View();
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+    }
+}
---
+++ FileUploadTest/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FileUploadTest.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string ImagePath { get; set; }
+        public HttpPostedFile ImageFile { get; set; }
+    }
+}
